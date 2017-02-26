b78fc820f4f645913273267a6febcc7972f2ea53
mbuzzas <mbuzzas3@gmail.com>
2017-02-26 12:00:18 -0800

    adds seedBlogPostData function

---
+++ test/test-blogs-integration.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const should = chai.should();
+
+const {BlogPost} = require('../models');
+const {DATABASE_URL} = require('../config');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+function seedBlogPostData() {
+	console.info('seeding blog post data');
+	const seedData = [];
+
+	for (let i=1; i<=10; i++) {
+		seedData.push({
+			author: {
+				firstName: faker.name.firstName(),
+				lastName: faker.name.lastName()
+			}
+			title: faker.lorem.sentence(),
+			content: faker.lorem.text()
+		});
+	}
+	return BlogPost.insertMany(seedData);
+}
+
+function tearDownDb
+
